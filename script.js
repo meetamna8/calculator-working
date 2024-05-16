649f0011c5e057e9398b0dafe61f813981e3a445
meetamna8 <140243713+meetamna8@users.noreply.github.com>
2024-05-16 23:46:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function Calculte() {
+      let a = document.getElementById("one").value;
+      let b = document.getElementById("two").value;
+      alert(parseInt(a) + parseInt(b))
+    }
+
+    function CalculteDiff() {
+      let a = document.getElementById("three").value;
+      let b = document.getElementById("four").value;
+      alert(parseInt(a) - parseInt(b));
+    }
+
+    function CalculteMulti() {
+      let a = document.getElementById("five").value;
+      let b = document.getElementById("six").value;
+      alert(parseInt(a) * parseInt(b));
+    }
